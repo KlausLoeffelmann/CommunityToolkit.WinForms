c9d927b68aad0212bebb50e9864ebc84519a622f
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2024-10-14 08:09:21 -0700

    Fix bugs in the ExtendedDebugInfo parser.

---
+++ tests/cs/CommunityToolkit.WinForms.TestsX/AsyncQueueTests.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.WinForms.AsyncSupport;
+using System.Diagnostics;
+
+namespace CommunityToolkit.WinForms.TestsX;
+
+public class AsyncQueueTests
+{
+    [Fact]
+    public void SimpleSyncCommandQueueTestAsync()
+    {
+        var queue = new AsyncTaskQueue();
+        queue.Enqueue(task);
+        queue.Dispose();
+
+        // Simplest local Func<Task> to enqueue.
+        static Task task() => Task.CompletedTask;
+    }
+
+    [Fact]
+    public void MultipleSimpleSyncCommandQueueTest()
+    {
+        int count = 0;
+
+        var queue = new AsyncTaskQueue(1000);
+        for (int i = 0; i < 1000; i++)
+        {
+            queue.Enqueue(task);
+        }
+
+        // Simplest local Func<Task> to enqueue.
+        Task task()
+        {
+            count++;
+            return Task.CompletedTask;
+        }
+    }
+}
